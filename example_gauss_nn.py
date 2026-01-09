62d7b8f0bc829094a784b706f787cb19fea518f2
debrevitatevitae <giotb92@gmail.com>
2026-01-09 09:42:00 +0100

    Add training loop to example.

--- example_gauss_nn.py
+++ example_gauss_nn.py
@@ -1,12 +1,18 @@
 import json
 
+import jax
 import lib_eod_simulation as les
+import optax
 from flax import nnx
 from grain import DataLoader
 from grain.samplers import IndexSampler
 from grain.transforms import Batch
 
-from qmodem import BatterySimulationSingleTimeSource, GaussianHeteroscedasticMLP
+from qmodem import (
+    BatterySimulationSingleTimeSource,
+    GaussianHeteroscedasticMLP,
+    nll_loss,
+)
 
 
 def quickstart_dataloader(N_simu: int = 1, batch_size: int = 10) -> DataLoader:
@@ -34,17 +40,49 @@ def quickstart_dataloader(N_simu: int = 1, batch_size: int = 10) -> DataLoader:
 
 
 def main() -> None:
-    # Run iid simulations for training and testing
+    LR = 1e-2
+    N_EPOCHS = 1_000
+    PRINT_EVERY = 50
+
+    rngs = nnx.Rngs(0)
+
+    # Run iid simulations for training and testing.
     dataloader_train = quickstart_dataloader(N_simu=40, batch_size=5)
     # dataloader_test = quickstart_dataloader(N_simu=10)
 
-    # Define the model
-    rngs = nnx.Rngs(0)
+    # Define the model.
     model = GaussianHeteroscedasticMLP(dimensions=[1, 30, 30, 30, 30], rngs=rngs)
 
-    # Run the model on a batch
-    batch = next(iter(dataloader_train))
-    print(model(batch[0]))
+    # Run the model on a batch.
+    # batch = next(iter(dataloader_train))
+    # print(model(batch[0]))
+
+    # Define the optimizer.
+    optimizer = nnx.Optimizer(model, optax.adam(learning_rate=LR), wrt=nnx.Param)
+
+    # Define a (jitted) training step function.
+    @nnx.jit
+    def train_step(
+        model: GaussianHeteroscedasticMLP,
+        optimizer: nnx.Optimizer,
+        rngs: nnx.Rngs,
+        batch: tuple[jax.Array],
+    ) -> jax.Array:
+        """One step of the training (parameter and optimizer state update)."""
+        grad_fn = nnx.value_and_grad(nll_loss, argnums=0, has_aux=False)
+        loss, grads = grad_fn(model, batch, rngs)
+        optimizer.update(model, grads)  # In-place updates.\
+        return loss
+
+    # Train the model.
+    model.train()
+
+    for epoch in range(N_EPOCHS):
+        for batch in dataloader_train:
+            loss = train_step(model, optimizer, rngs, batch)
+
+        if (epoch + 1) % PRINT_EVERY == 0:
+            print(f"Epoch: {epoch:3d}, loss: {loss:.4f}")
 
 
 if __name__ == "__main__":
