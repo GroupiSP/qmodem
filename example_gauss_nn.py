e6ee8748b5d7d8e77c7c8e0df20e7c7d58fc9096
debrevitatevitae <giotb92@gmail.com>
2026-01-12 11:48:36 +0100

    Add test DS and eval step.

--- example_gauss_nn.py
+++ example_gauss_nn.py
@@ -11,15 +11,17 @@ from qmodem import GaussianHeteroscedasticMLP, make_battery_datasource, nll_loss
 def main() -> None:
     LR = 1e-2
     N_EPOCHS = 100
-    PRINT_EVERY = 5
+    PRINT_EVERY = 10
     N_SIMU_TRAIN_DS = 10
-    # N_SIMU_TEST_DS = 5
+    N_SIMU_TEST_DS = 5
     BATCH_SIZE = 50
 
     rngs = nnx.Rngs(0)
 
     # Run iid simulations for training and testing.
     ds_train = make_battery_datasource(N_simu=N_SIMU_TRAIN_DS)
+    ds_test = make_battery_datasource(N_simu=N_SIMU_TEST_DS)
+
     sampler_train = IndexSampler(
         num_records=len(ds_train), num_epochs=1, shuffle=True, seed=0
     )
@@ -36,29 +38,42 @@ def main() -> None:
     # Define the optimizer.
     optimizer = nnx.Optimizer(model, optax.adam(learning_rate=LR), wrt=nnx.Param)
 
-    # Define a (jitted) training step function.
+    # Define (jitted) training step and test step functions.
     @nnx.jit
     def train_step(
         model: GaussianHeteroscedasticMLP,
         optimizer: nnx.Optimizer,
         rngs: nnx.Rngs,
         batch: tuple[jax.Array],
-    ) -> jax.Array:
+    ) -> None:
         """One step of the training (parameter and optimizer state update)."""
         grad_fn = nnx.value_and_grad(nll_loss, argnums=0, has_aux=False)
         loss, grads = grad_fn(model, batch, rngs)
         optimizer.update(model, grads)  # In-place updates.\
-        return loss
+
+    @nnx.jit
+    def eval_step(
+        model: GaussianHeteroscedasticMLP, rngs: nnx.Rngs, dataset: tuple[jax.Array]
+    ) -> jax.Array:
+        """Evaluates the model over the entire data-source."""
+        return nll_loss(model, batch=dataset, rngs=rngs)
 
     # Train the model.
-    model.train()
+    for epoch in range(1, N_EPOCHS + 1):
+        model.train()
 
-    for epoch in range(N_EPOCHS):
         for batch in dataloader_train:
-            loss = train_step(model, optimizer, rngs, batch)
+            train_step(model, optimizer, rngs, batch)
 
-        if (epoch + 1) % PRINT_EVERY == 0:
-            print(f"Epoch: {epoch:3d}, loss: {loss:.4f}")
+        if epoch % PRINT_EVERY == 0:
+            model.eval()
+
+            train_ds_loss = eval_step(model, rngs, ds_train[:])
+            test_ds_loss = eval_step(model, rngs, ds_test[:])
+
+            print(
+                f"Epoch: {epoch:3d}, train loss: {train_ds_loss:.4f}, test loss: {test_ds_loss:.4f}"
+            )
 
 
 if __name__ == "__main__":
