66f79a09a3cb48e1384c608177c4cb627ee51a62
debrevitatevitae <giotb92@gmail.com>
2026-01-28 09:22:54 +0100

    Refactor test functions to use consistent mock_hnn_v0 naming

--- tests/test_model.py
+++ tests/test_model.py
@@ -9,21 +9,21 @@ _rng_key = jax.random.PRNGKey(0)
 
 
 @pytest.fixture
-def mock_gauss_het_mlp() -> HNNV0:
+def mock_hnn_v0() -> HNNV0:
     return HNNV0(dimensions=[1, 10, 10], rngs=nnx.Rngs(0))
 
 
-def test_gauss_het_mlp_init(mock_gauss_het_mlp) -> None:
+def test_hnn_v0_init(mock_hnn_v0) -> None:
     """Tests the __init__ of the GaussianHeteroscedasticMLP.
 
     It checks:
     - if the number of linear layers is the expected one.
     """
     num_linear_layers = sum(
-        isinstance(layer, nnx.Linear) for _, layer in mock_gauss_het_mlp.iter_modules()
+        isinstance(layer, nnx.Linear) for _, layer in mock_hnn_v0.iter_modules()
     )
     # The Gaussian layer contains 2 linear layers.
-    assert num_linear_layers == mock_gauss_het_mlp.n_hid_layers + 2
+    assert num_linear_layers == mock_hnn_v0.n_hid_layers + 2
 
 
 @pytest.mark.parametrize(
@@ -34,7 +34,7 @@ def test_gauss_het_mlp_init(mock_gauss_het_mlp) -> None:
         jnp.zeros(shape=[10, 1]),
     ],
 )
-def test_gauss_het_mlp_forward(mock_gauss_het_mlp, x) -> None:
+def test_hnn_v0_fwd(mock_hnn_v0, x) -> None:
     """Tests the __call__ of the GaussianHeteroscedasticMLP. The test is parametrized
     for different input types. Batch size generic, batch size = 1, all zeros.
 
@@ -43,7 +43,7 @@ def test_gauss_het_mlp_forward(mock_gauss_het_mlp, x) -> None:
     - the output shape
     - if the variance output is always non-negative
     """
-    preds = mock_gauss_het_mlp(x)
+    preds = mock_hnn_v0(x)
     assert isinstance(preds, jax.Array)
     assert preds.shape == (x.shape[0], 2)
     assert jnp.all(preds[:, 1] >= 0.0)
@@ -72,7 +72,7 @@ def test_gauss_het_mlp_forward(mock_gauss_het_mlp, x) -> None:
         ),
     ],
 )
-def test_nll_loss(mock_gauss_het_mlp, batch) -> None:
+def test_nll_loss(mock_hnn_v0, batch) -> None:
     """Tests the negative log-likelihood loss. The test is parametrized for different
     input types. Batch size generic, batch size = 1, all zeros.
 
@@ -80,6 +80,6 @@ def test_nll_loss(mock_gauss_het_mlp, batch) -> None:
     - if the output is a jax array
     - if the output contains one element (shape=(1,))
     """
-    loss_value = nll_loss(mock_gauss_het_mlp, batch)
+    loss_value = nll_loss(mock_hnn_v0, batch)
     assert isinstance(loss_value, jax.Array)
     assert jnp.isscalar(loss_value)
