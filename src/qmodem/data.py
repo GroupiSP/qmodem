dadbad233b2be5e6475e160a9500ed9953824344
debrevitatevitae <giotb92@gmail.com>
2026-02-12 14:53:23 +0100

    Remove unused BatterySimulationTimeSeriesSource class and its methods

--- src/qmodem/data.py
+++ src/qmodem/data.py
@@ -176,44 +176,6 @@ class BatterySimulationTimeWindowSource:
         return self.X[record_key], self.y[record_key]
 
 
-# TODO: remove
-class BatterySimulationTimeSeriesSource:
-    def __init__(
-        self,
-        simulator: les.SimulatorSimple | les.SimulatorComplete,
-    ) -> None:
-        """Runs and access to battery simulation data as time series.
-
-        Args:
-            simulator (les.SimulatorSimple | les.SimulatorComplete): the simulator from
-                lib_eod_simulation. It needs to be configured outside of this data
-                source.
-        """
-        simulator.simulate()
-
-        # Transpose for convenience. Shape=(N_simu, N_t).
-        discharge_voltage_per_sim: np.ndarray = simulator.v_memo.T
-
-        self.X = jnp.array(discharge_voltage_per_sim)
-        self.y = jnp.array(simulator.t_eods)
-
-    def __len__(self) -> int:
-        """Number of records in the dataset, corresponding to the number of discharge
-        histories."""
-        return len(self.y)
-
-    def __getitem__(self, record_key: SupportsIndex) -> tuple[jax.Array, float]:
-        """Retrieves record for the given record_key."""
-        return self.X[record_key], self.y[record_key]
-
-    def shuffle(self, seed: int) -> None:
-        """Shuffles the order of the records in the dataset."""
-        key = jax.random.PRNGKey(seed)
-        perm = jax.random.permutation(key, len(self.y))
-        self.X = self.X[perm]
-        self.y = self.y[perm]
-
-
 class CombinedTimeWindowSource:
     def __init__(self, sources: list[BatterySimulationTimeWindowSource]) -> None:
         """Combined data source from multiple time window sources.
