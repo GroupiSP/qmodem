e80391baebeded571d00f9134cb9b4d654a88388
debrevitatevitae <giotb92@gmail.com>
2026-05-13 16:18:42 +0200

    [scripts/train-hnn] Create the three datasources

--- scripts/battery/train_hnn.py
+++ scripts/battery/train_hnn.py
@@ -1,8 +1,17 @@
 from __future__ import annotations
 
 import pathlib
+from typing import Sequence
 
 from qmodem.data import DataFrameSource, make_battery_data_pipeline
+
+
+def get_data_paths(data_dir: pathlib.Path) -> tuple[Sequence[pathlib.Path]]:
+    all_tv_paths = sorted(data_dir.glob("train_history_[0-9]*.csv"))
+    training_paths = [p for p in all_tv_paths if int(p.stem.split("_")[-1]) < 100]
+    validation_paths = [p for p in all_tv_paths if int(p.stem.split("_")[-1]) >= 100]
+    test_paths = sorted(data_dir.glob("test_history_[0-9]*.csv"))
+    return training_paths, validation_paths, test_paths
 
 
 def main() -> None:
@@ -13,10 +22,14 @@ def main() -> None:
         / "battery"
     )
 
-    # Process training data
-    training_paths = RAW_DATA_DIR.glob("train_history_[0-9]*.csv")
+    training_paths, validation_paths, test_paths = get_data_paths(RAW_DATA_DIR)
 
-    DataFrameSource(paths=list(training_paths), pipeline=make_battery_data_pipeline())
+    # Build the data sources, including windowing and normalization
+    data_pipeline = make_battery_data_pipeline(window_size=20, stride=1, normalize=True)
+
+    DataFrameSource(paths=list(training_paths), pipeline=data_pipeline)
+    DataFrameSource(paths=list(validation_paths), pipeline=data_pipeline)
+    DataFrameSource(paths=list(test_paths), pipeline=data_pipeline)
 
 
 if __name__ == "__main__":
