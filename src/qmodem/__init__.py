0a2f5e2a653552c817bafd8787c01d4b75208ea7
debrevitatevitae <giotb92@gmail.com>
2026-06-12 15:11:54 +0200

    [refac/clean-up] Empty __init__

--- src/qmodem/__init__.py
+++ src/qmodem/__init__.py
@@ -1,50 +1,6 @@
-from .data import (  # noqa: F401
-    CMAPSSAnalyst,
-    CMAPSSDataSource,
-    create_dataloaders,
-    split_cmapss,
-)
-from .metadata import (  # noqa: F401
-    BaseModelParams,
-    MCDModelParams,
-    PQCParams,
-    QAVITrainingMetadata,
-    QAVITrainingParams,
-    ScalingParams,
-    SimulatorConfig,
-    TrainingMetadata,
-    TrainingParams,
-    load_metadata,
-    save_metadata,
-)
-from .module import (  # noqa: F401
-    HNNV0,
-    HNNV1,
-    LSTM,
-    MCDCNN1D,
-    BayesCNN1D,
-    FlipoutConv1D,
-    HeteroscedasticCNN1D,
-    HeteroscedasticCNN1DV1,
-    MCDNetV0,
-    MCDNetV1,
-    SimpleCNN1D,
-    StandardBayesConv1D,
-    elbo_nll_loss,
-    nll_loss,
-)
-from .utils import (  # noqa: F401
-    BATT_CONFIG_PATH,
-    SHARED_PARAMS,
-    TEST_SEED,
-    TRAIN_SEED,
-    create_battery_and_policy,
-    get_run_dirs,
-    make_simulator_config,
-    restore_model_from_checkpoint,
-    restore_model_state,
-)
-
-
 def main() -> None:
     print("Hello from qmodem!")
+
+
+if __name__ == "__main__":
+    main()
