adb9db1d57f9f461cece0e2595d1d8abcbd2d748
Giorgio Tosti Balducci <64412197+debrevitatevitae@users.noreply.github.com>
2026-07-23 09:23:05 +0200

    Battery ECM: variable loading conditions (#67)
    * [scripts/generate] Add option 4 variable current loading
    
    * [deps] uv lock upgrade
    
    * [scripts] Add first code (generation) for battery with multiple loading scenarios
    
    * [fix/update; scripts] Adapt single load data generation to new version of simbat
    
    * [scripts] Add generation and HNNs for the variable loading scenarios
    
    Also make the necessary changes, exp. to match sampling frequency and window size and to update the simulator.
    
    * [scripts/battery_variable/hnn] Rename indices for SoC_0s
    
    * [data generation] Increase -2.0 duration of variable cruise profile and increase sigma_SoC
    
    * [scripts/hnn] Accept incomplete time windows at early times during testing
    
    And control the number of eval SoCs explicitly
    
    * [scripts/hnn] Single legend for RUL subplots
    
    * [scripts/battery variable loading] Add MCD
    
    * [scripts/battery variable loading] Increase ES patience once and for all
    
    * [scripts/battery variable/hnn] Inject act-fn at Net init time
    
    * [scripts/battery variable loading] Add BNN scripts
    
    * [scripts/battery variable loading] Add QAVI
    
    * [module/NLL] Correct beta-NLL
    
    * [feat/fix] beta NLL loss with rescaling
    
    * [scripts/variable loading/hnn] Runs for beta=0.5

--- scripts/battery_multiple_scenarios/generate_discharge_histories.py
+++ scripts/battery_multiple_scenarios/generate_discharge_histories.py
@@ -12,32 +12,17 @@ import pandas as pd
 import simbat as sb
 
 from qmodem.tracking import MLFlowSetup, track_mlflow
-
-
-class VariableDischargeCurrentPolicy:
-    def __init__(self, current_values: list[float], time_values: list[float]) -> None:
-        self.current_values = current_values
-        self.time_values = time_values
-
-    def __call__(self, soc: float, t: float) -> float:
-        """Returns the current values at time `t` for the given SoC values."""
-        for i in range(len(self.time_values) - 1):
-            if self.time_values[i] <= t < self.time_values[i + 1]:
-                return self.current_values[i]
-        return self.current_values[
-            -1
-        ]  # Return the last current value if t exceeds the last time value
+from scripts.battery_multiple_scenarios.commons import (
+    RAW_DATA_DIR,
+    constant_cruise_policy,
+    variable_cruise_policy,
+)
 
 
 class ProcessNoiseDistribution(StrEnum):
     NORMAL = auto()
     UNIFORM = auto()
     ZERO = auto()
-
-
-class CurrentPolicy(StrEnum):
-    CONSTANT = auto()
-    VARIABLE = auto()
 
 
 class VOCModel(StrEnum):
@@ -54,17 +39,6 @@ dist_name_to_params = {
     ProcessNoiseDistribution.ZERO: lambda: {},
 }
 
-name_to_policy = {
-    # Current values in Amperes
-    CurrentPolicy.CONSTANT: sb.simulate.ConstantCurrentDischarge(
-        current_value=-2.8 * 0.75
-    ),
-    CurrentPolicy.VARIABLE: VariableDischargeCurrentPolicy(
-        current_values=[-2.0, -1.0, -4.0, -2.0, -3.0],
-        time_values=[0.0, 600.0, 900.0, 1800.0, 3000.0],
-    ),
-}
-
 ecm_model_name_to_params = {
     ECMModel.THEVENIN_ZERO_ORDER: lambda r0: {"r0": r0},
 }
@@ -72,7 +46,6 @@ ecm_model_name_to_params = {
 
 @dataclass(frozen=True)
 class Hyperparameters:
-    current_policy: CurrentPolicy = CurrentPolicy.CONSTANT
     voc_model: VOCModel = VOCModel.BUSTOS_BAEZA
     ecm_model: ECMModel = ECMModel.THEVENIN_ZERO_ORDER
     ecm_model_params: dict[str, float] = field(
@@ -81,9 +54,9 @@ class Hyperparameters:
         )
     )
     battery_nominal_capacity: float = 10080.0  # in Coulombs
-    dt: float = 20.0
+    dt: float = 60.0
     v_cutoff: float = 2.5  # in Volts
-    n_histories_train: int = 100
+    n_histories_train: int = 50
     n_histories_val: int = 20
     n_histories_test: int = 10
     process_noise_distribution: ProcessNoiseDistribution = (
@@ -95,7 +68,7 @@ class Hyperparameters:
     # TODO: noise parameter tracking can be improved by using `dist_name_to_params`. But it
     # is not a prio right now.
     process_noise_loc: float = 0.0
-    process_noise_std: float = 3e-3
+    process_noise_std: float = 5e-3
     measurement_noise_param: Any = None
     soc_range_train_val: tuple[float, float] = (0.05, 1.0)
     train_seed: int = 42
@@ -115,29 +88,38 @@ def _modify_dataframe(df: pd.DataFrame, run_id: int) -> None:
 
 
 def generate_train(rng: np.random.Generator, hp: Hyperparameters) -> pd.DataFrame:
-    soc_0s = rng.uniform(
-        low=hp.soc_range_train_val[0],
-        high=hp.soc_range_train_val[1],
-        size=hp.n_histories_train + hp.n_histories_val,
-    )
+    """Generate training and validation discharge histories.
+
+    A first initial Monte Carlo simulation is run to generate a set of initial SoC
+    values and record the intermediate times. Then, for each initial SoC, a discharge
+    history is generated using the same simulation parameters.
+    """
+    current_policies = [constant_cruise_policy, variable_cruise_policy]
+
+    def policy_choice_distribution():
+        return rng.choice([0, 1], p=[0.7, 0.3])
+
+    def process_noise_distribution():
+        return rng.normal(loc=hp.process_noise_loc, scale=hp.process_noise_std)
+
+    def measurement_noise_distribution():
+        return 0.0
 
     out_df = pd.DataFrame(columns=["run_id", "time", "soc", "voltage"])
 
-    for i, soc_0 in enumerate(soc_0s):
+    for i in range(hp.n_histories_train + hp.n_histories_val):
         config = sb.SimulationConfig(
-            current_policies=[name_to_policy[hp.current_policy]],
-            policy_choice_distribution=lambda: 0,  # Always choose the first policy
-            process_noise_distribution=lambda: rng.normal(
-                loc=hp.process_noise_loc, scale=hp.process_noise_std
-            ),
-            measurement_noise_distribution=lambda: 0.0,
+            current_policies=current_policies,
+            policy_choice_distribution=policy_choice_distribution,
+            process_noise_distribution=process_noise_distribution,
+            measurement_noise_distribution=measurement_noise_distribution,
             dt=hp.dt,
-            soc_0=soc_0,
+            soc_0=1.0,
         )
+
         result = sb.simulate_constant_capacity_simple(n_sim=1, config=config)
         df = result.to_dataframe()
 
-        # Modify the dataframe and append it to the output one
         _modify_dataframe(df, i)
 
         out_df = pd.concat([out_df, df], ignore_index=True)
@@ -146,21 +128,30 @@ def generate_train(rng: np.random.Generator, hp: Hyperparameters) -> pd.DataFram
 
 
 def generate_test(rng: np.random.Generator, hp: Hyperparameters) -> pd.DataFrame:
+    # TODO: This function is very similar to `generate_train`. We can refactor it to avoid code duplication.
+    current_policies = [constant_cruise_policy, variable_cruise_policy]
+
+    def policy_choice_distribution():
+        return rng.choice([0, 1], p=[0.7, 0.3])
+
+    def process_noise_distribution():
+        return rng.normal(loc=hp.process_noise_loc, scale=hp.process_noise_std)
+
+    def measurement_noise_distribution():
+        return 0.0
+
     out_df = pd.DataFrame(columns=["run_id", "time", "soc", "voltage"])
 
     for i in range(hp.n_histories_test):
         config = sb.SimulationConfig(
-            current_policies=[name_to_policy[hp.current_policy]],
-            policy_choice_distribution=lambda: (
-                0
-            ),  # Always choose the first policy (only one policy in this case)
-            process_noise_distribution=lambda: rng.normal(
-                loc=hp.process_noise_loc, scale=hp.process_noise_std
-            ),
-            measurement_noise_distribution=lambda: 0.0,
+            current_policies=current_policies,
+            policy_choice_distribution=policy_choice_distribution,
+            process_noise_distribution=process_noise_distribution,
+            measurement_noise_distribution=measurement_noise_distribution,
             dt=hp.dt,
             soc_0=1.0,
         )
+
         result = sb.simulate_constant_capacity_simple(n_sim=1, config=config)
         df = result.to_dataframe()
 
@@ -171,21 +162,13 @@ def generate_test(rng: np.random.Generator, hp: Hyperparameters) -> pd.DataFrame
     return out_df
 
 
-def plot_current_policy(ax: plt.Axes, hp: Hyperparameters) -> None:
-    t_grid = np.linspace(0, 5000, 100)
-    current_values = [name_to_policy[hp.current_policy](soc=None, t=t) for t in t_grid]
-    ax.plot(t_grid, current_values)
-    ax.set_xlabel("Time")
-    ax.set_ylabel("Current")
-    ax.grid()
-
-
 def run_sims_from_tzero(
     rng: np.random.Generator, hp: Hyperparameters
 ) -> sb.SimulationResult:
+    # TODO: This function is very similar to `generate_train`. We can refactor it to avoid code duplication.
     config = sb.SimulationConfig(
-        current_policies=[name_to_policy[hp.current_policy]],
-        policy_choice_distribution=lambda: 0,  # Always choose the first policy
+        current_policies=[constant_cruise_policy, variable_cruise_policy],
+        policy_choice_distribution=lambda: rng.choice([0, 1], p=[0.7, 0.3]),
         process_noise_distribution=lambda: rng.normal(
             loc=hp.process_noise_loc, scale=hp.process_noise_std
         ),
@@ -211,23 +194,14 @@ def main() -> None:
 
     Train/validation data generation:
     - 100 discharge histories for training, 20 for validation.
-    - Initial SoC sampled uniformly from [0.05, 1.0].
     - Same RNG for reproducibility.
 
     Test data generation:
     - 10 test cases.
     - Different RNG to ensure independent test data.
     """
-    BATTERY_DATA_DIR = (
-        pathlib.Path(__file__).resolve().parent.parent.parent
-        / "data"
-        / "raw"
-        / "battery"
-    )
 
-    hp = Hyperparameters(
-        current_policy=CurrentPolicy.VARIABLE,
-    )
+    hp = Hyperparameters()
 
     # MLFlow setup
     run_tags = {
@@ -235,8 +209,8 @@ def main() -> None:
         "stage": "data_generation",
     }
     tracking_setup = MLFlowSetup(
-        experiment_name="reliability_study",
-        run_name="generate_discharge_histories",
+        experiment_name="variable_loading_conditions",
+        run_name="generate_discharge_histories_multiple_policies",
         tags=run_tags,
     )
     with track_mlflow(tracking_setup):
@@ -248,15 +222,20 @@ def main() -> None:
         train_df = generate_train(train_rng, hp)
         test_df = generate_test(test_rng, hp)
 
-        save_dataframe_to_file(train_df, path=BATTERY_DATA_DIR / "train.csv")
-        save_dataframe_to_file(test_df, path=BATTERY_DATA_DIR / "test.csv")
+        save_dataframe_to_file(train_df, path=RAW_DATA_DIR / "train.csv")
+        save_dataframe_to_file(test_df, path=RAW_DATA_DIR / "test.csv")
 
         track_dataframe(train_df, name="battery_train", context="train")
         track_dataframe(test_df, name="battery_test", context="test")
 
-        fig, ax = plt.subplots(figsize=(8, 6))
-        plot_current_policy(ax=ax, hp=hp)
-        mlflow.log_figure(fig, artifact_file="current_profile.png")
+        fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
+
+        constant_cruise_policy.plot(ax=axs[0])
+        axs[0].set_title("Constant Cruise Policy")
+
+        variable_cruise_policy.plot(ax=axs[1])
+        axs[1].set_title("Variable Cruise Policy")
+        mlflow.log_figure(fig, artifact_file="current_profiles.png")
 
         res_for_plots = run_sims_from_tzero(rng=train_rng, hp=hp)
 
@@ -267,6 +246,19 @@ def main() -> None:
         sb.plot_soc_results(ax=axs[3], results=[res_for_plots])
 
         mlflow.log_figure(fig, artifact_file="discharge_results.png")
+
+        # Make another plot for the voltage discharge of the first 10 test cases
+        fig, axs = plt.subplots(nrows=2, ncols=5, figsize=(20, 10))
+        for i in range(10):
+            ax = axs[i // 5, i % 5]
+            test_case_df = test_df[test_df["run_id"] == i]
+            ax.plot(test_case_df["time"], test_case_df["voltage"])
+            ax.set_title(f"Test Case {i}")
+            ax.set_xlabel("Time (s)")
+            ax.set_ylabel("Voltage (V)")
+            ax.grid()
+        fig.tight_layout()
+        mlflow.log_figure(fig, artifact_file="test_case_voltage_discharge.png")
 
 
 if __name__ == "__main__":
---
+++ scripts/battery_multiple_scenarios/hnn_train.py
@@ -0,0 +1,199 @@
+from __future__ import annotations
+
+import dataclasses
+import functools
+import io
+import logging
+
+import flax.nnx as nnx
+import jax
+import jax.numpy as jnp
+import mlflow
+import optax
+import sklearn.preprocessing as skpp
+
+from qmodem.data import (
+    DataFrameSource,
+    DataPipeline,
+    add_feature_dimension_to_y,
+    get_time_windows_and_join,
+    normalize_ruls,
+    to_jax,
+)
+from qmodem.module import nll_batched
+from qmodem.tracking import (
+    MLFlowSetup,
+    track_mlflow,
+)
+from qmodem.train import (
+    LogReporter,
+    mlflow_track_losses,
+    train_loop,
+)
+from qmodem.train_base import (
+    EarlyStopper,
+    OutputVarianceTracker,
+    mlflow_track_model_best_state,
+)
+from qmodem.utils import count_parameters
+from scripts.battery_multiple_scenarios.commons import (
+    DATA_GEN_RUN_ID,
+    RAW_DATA_DIR,
+    TrainHyperparameters,
+    get_dataframes,
+    train_dataloader_builder,
+)
+from scripts.battery_multiple_scenarios.hnn_model import Net
+
+
+def main() -> None:
+    log_stream = io.StringIO()
+    logging.basicConfig(
+        level=logging.INFO,
+        force=True,
+        handlers=[
+            logging.StreamHandler(),  # console (stderr)
+            logging.StreamHandler(log_stream),  # in-memory stream for MLflow logging
+        ],
+    )
+
+    hp = TrainHyperparameters(beta_nll=0.5, early_stopping_patience=50)
+
+    mlflow_setup = MLFlowSetup(
+        run_name="hnn-3",
+        experiment_name="variable_loading_conditions",
+        run_description="""Beta=0.5 NLL.""",
+        tags={
+            "model": "HNN",
+            "case_study": "battery",
+            "stage": "prototyping",
+        },
+    )
+
+    # Model, schedule, optimizer
+    model = Net(
+        rngs=nnx.Rngs(hp.net_init_seed), act_fn=getattr(nnx, hp.activation_function)
+    )
+
+    # Build the data sources, including windowing and normalization
+    scaler = skpp.MinMaxScaler(feature_range=(0, 1))
+    data_pipeline_train = DataPipeline(
+        [
+            functools.partial(
+                get_time_windows_and_join,
+                window_size=hp.window_size,
+                stride=hp.stride,
+            ),
+            add_feature_dimension_to_y,
+            functools.partial(normalize_ruls, transform_fn=scaler.fit_transform)
+            if hp.normalize_rul
+            else lambda x: x,
+            to_jax,
+        ]
+    )
+    data_pipeline_val = DataPipeline(
+        [
+            functools.partial(
+                get_time_windows_and_join,
+                window_size=hp.window_size,
+                stride=hp.stride,
+            ),
+            add_feature_dimension_to_y,
+            functools.partial(normalize_ruls, transform_fn=scaler.transform)
+            if hp.normalize_rul
+            else lambda x: x,
+            to_jax,
+        ]
+    )
+    data_gen_run = mlflow.get_run(DATA_GEN_RUN_ID)
+    train_df, val_df, _ = get_dataframes(
+        RAW_DATA_DIR / "train.csv",
+        RAW_DATA_DIR / "test.csv",
+        n_histories_train=int(data_gen_run.data.params["n_histories_train"]),
+    )
+
+    ds_train = DataFrameSource(df=train_df, pipeline=data_pipeline_train)
+    ds_val = DataFrameSource(df=val_df, pipeline=data_pipeline_val)
+
+    @nnx.jit
+    def train_step(
+        model: nnx.Module,
+        batch: tuple[jax.Array, jax.Array],
+        keys: jax.Array,
+        optimizer: nnx.Optimizer,
+    ) -> jax.Array:
+        def loss_fn(model):
+            return jnp.mean(nll_batched(model, batch, keys, beta=hp.beta_nll))
+
+        loss, grads = nnx.value_and_grad(loss_fn)(model)
+        optimizer.update(model, grads)
+        return loss
+
+    @nnx.jit
+    def eval_step(
+        model: nnx.Module,
+        batch: tuple[jax.Array, jax.Array],
+        keys: jax.Array,
+        optimizer: nnx.Optimizer = None,  # not used, but we keep the same signature as train_step for simplicity
+    ) -> jax.Array:
+        return jnp.mean(nll_batched(model, batch, keys, beta=0.0))
+
+    schedule = optax.cosine_decay_schedule(
+        init_value=hp.learning_rate,
+        decay_steps=hp.n_epochs * (len(ds_train) // hp.batch_size),
+        alpha=hp.scheduler_alpha,
+    )
+    optimizer = nnx.Optimizer(model, optax.adam(schedule), wrt=nnx.Param)
+
+    early_stopper = EarlyStopper(
+        patience=hp.early_stopping_patience, min_delta=hp.early_stopping_min_delta
+    )
+
+    with track_mlflow(setup=mlflow_setup):
+        mlflow.sklearn.log_model(scaler, artifact_path="sklearn_scaler")
+        mlflow.log_params(dataclasses.asdict(hp))
+        mlflow.log_param("n_params", count_parameters(model))
+
+        key = jax.random.key(hp.train_rng_seed)
+        key, subkey = jax.random.split(key)
+
+        batch_variance_tracking = ds_val[
+            jax.random.choice(
+                subkey, len(ds_val), shape=(hp.batch_size,), replace=False
+            )
+        ]
+
+        train_loop(
+            n_epochs=hp.n_epochs,
+            train_dataloader_builder=functools.partial(
+                train_dataloader_builder,
+                ds_train=ds_train,
+                batch_size=hp.batch_size,
+                drop_remainder=hp.drop_remainder,
+            ),
+            val_dataloader_builder=lambda n: [
+                (ds_val.X, ds_val.y)
+            ],  # single "batch" = whole val set, because no SGD happens at eval time
+            initial_key=key,
+            model=model,
+            optimizer=optimizer,
+            train_batch_fn=train_step,
+            eval_batch_fn=eval_step,
+            callbacks=[
+                LogReporter(log_every=10),
+                mlflow_track_model_best_state,
+                mlflow_track_losses,
+                OutputVarianceTracker(
+                    base_key=subkey,
+                    X_batch=batch_variance_tracking[0],
+                    n_samples=hp.n_samples_predictive_mean_variance,
+                ),
+            ],
+            early_stopper=early_stopper,
+        )
+
+        mlflow.log_text(log_stream.getvalue(), "training_log.txt")
+
+
+if __name__ == "__main__":
+    main()
