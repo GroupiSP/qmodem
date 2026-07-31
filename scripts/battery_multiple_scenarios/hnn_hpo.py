a0e51416b864c614c2618e2e4c382f7040462a8b
debrevitatevitae <giotb92@gmail.com>
2026-07-31 09:25:30 +0200

    Finalize HNN HPO script

--- scripts/battery_multiple_scenarios/hnn_hpo.py
+++ scripts/battery_multiple_scenarios/hnn_hpo.py
@@ -1,55 +1,88 @@
 from __future__ import annotations
 
+import io
+import logging
 import os
 import pathlib
 
 import flax.nnx as nnx
+import mlflow
 import optuna
 from dotenv import load_dotenv
 
 from qmodem.battery.hpo import HPOHyperparameters, score_avg_val_crps
 from qmodem.battery.models import HeteroscedasticCNN
 from qmodem.battery.train import TrainHyperparameters, run_training
-from qmodem.tracking import MLFlowSetup
+from qmodem.tracking import MLFlowSetup, track_mlflow
 from qmodem.utils import setup_script_logging
 
 
-def objective(trial: optuna.Trial) -> float:
-    load_dotenv(override=True)
+def objective_factory(hp_hpo: HPOHyperparameters, log_stream: io.StringIO) -> float:
+    def objective(trial: optuna.Trial) -> float:
+        with mlflow.start_run(run_name=f"trial_{trial.number}", nested=True):
+            mlflow.log_params(trial.params)
 
-    log_stream = setup_script_logging()
-    hp_train = TrainHyperparameters()
-    hp_objective = HPOHyperparameters()
+            hp_train = TrainHyperparameters(
+                conv_kernel_size=trial.suggest_int("conv_kernel_size", 3, 10),
+                conv_n_filters=trial.suggest_int("conv_n_filters", 4, 40),
+                window_size=trial.suggest_int("window_size", 10, 100),
+                beta_nll=trial.suggest_float("beta_nll", 0.0, 1.0),
+                learning_rate=trial.suggest_float(
+                    "learning_rate", 1e-4, 1e-2, log=True
+                ),
+            )
 
-    mlflow_setup = MLFlowSetup(run_name="dummy_hnn_multiple")
-    model = HeteroscedasticCNN(
-        n_filters=hp_train.conv_n_filters,
-        kernel_size=hp_train.conv_kernel_size,
-        act_fn=getattr(nnx, hp_train.activation_function),
-        rngs=nnx.Rngs(hp_train.net_init_seed),
-    )
+            model = HeteroscedasticCNN(
+                n_filters=hp_train.conv_n_filters,
+                kernel_size=hp_train.conv_kernel_size,
+                act_fn=getattr(nnx, hp_train.activation_function),
+                rngs=nnx.Rngs(hp_train.net_init_seed),
+            )
 
-    run_training(
-        model=model,
-        hp=hp_train,
-        mlflow_setup=mlflow_setup,
-        raw_data_dir=pathlib.Path(os.environ["RAW_DATA_DIR_MULTI"]),
-        data_gen_run_id=os.environ["DATA_GEN_RUN_ID_MULTI"],
-        log_stream=log_stream,
-    )
+            run_training(
+                model=model,
+                hp=hp_train,
+                raw_data_dir=pathlib.Path(os.environ["RAW_DATA_DIR_MULTI"]),
+                data_gen_run_id=os.environ["DATA_GEN_RUN_ID_MULTI"],
+                log_stream=log_stream,
+                mlflow_setup=None,  # Setup happens at the HPO level
+            )
 
-    return score_avg_val_crps(
-        model=model,
-        mlflow_setup=mlflow_setup,
-        hp=hp_objective,
-        raw_data_dir=pathlib.Path(os.environ["RAW_DATA_DIR_MULTI"]),
-        validation_history_ids=list(range(50, 70)),
-        window_size=hp_train.window_size,
-    )
+            return score_avg_val_crps(
+                model=model,
+                hp=hp_hpo,
+                raw_data_dir=pathlib.Path(os.environ["RAW_DATA_DIR_MULTI"]),
+                validation_history_ids=list(range(50, 70)),
+            )
+
+    return objective
 
 
 def main() -> None:
-    pass
+    load_dotenv(override=True)
+
+    mlflow_setup = MLFlowSetup(run_name="hnn_hpo_1")
+    hp = HPOHyperparameters()
+    log_stream = setup_script_logging()
+
+    with track_mlflow(mlflow_setup):
+        hp_sampler = optuna.samplers.TPESampler(seed=hp.seed_hp_sampler)
+        study = optuna.create_study(sampler=hp_sampler, direction="minimize")
+        study.optimize(
+            func=objective_factory(hp, log_stream),
+            n_trials=hp.num_hp_trials,
+        )
+
+        logging.info("Best trial:")
+        best_trial = study.best_trial
+
+        logging.info(f"Value: {best_trial.value}")
+        logging.info("Params: ")
+        for key, value in best_trial.params.items():
+            logging.info(f"{key}: {value}")
+
+        mlflow.log_params(best_trial.params)  # log best trial params to the parent run
+        mlflow.log_text(log_stream.getvalue(), "hpo_log.txt")
 
 
 if __name__ == "__main__":
